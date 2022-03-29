da7a41563033064ddbccbea2675005af22a9e45c
cuong25 <cuonggaming@gmail.com>
2022-03-29 12:54:07 +0700

    lab02

---
+++ lab02.py
@@ -0,0 +1,59 @@
+students = []
+id = []
+mark = []
+course = []
+dob = []
+ 
+class Student:
+    def number_of_students():
+        number_of_students = x
+        x = int(input("Number of students: "))
+        return X
+    def students_information(number_of_students):
+        ID = input(x1)
+        Name = input(n)
+        Dob = input(d)
+        x1 = "ID: "
+        n = "Name: "
+        d = "Date of birth: "
+        for d in range (1900,2022):
+            print("Wrong date of birth(1900-2022)")
+        else:
+            return d
+    def show_students_information():
+        print("Number of students: "+students[i].get("x"))
+        for i in range (0,len(Mark)):
+            print("ID: "+ id[i].get("x1"))
+            print("Name: "+students[i].get("n"))
+            print("Date of birht: "+dob[i].get("d"))
+
+class Course:
+    def course_number():
+        course_number = y
+        y = int(input("Input course number: "))
+        return y
+    def course_information():
+        course_id = input(c)
+        course_name = input(cn)
+        c = "Course ID"
+        cn = "Course name"
+        return c, cn
+    def show_course_information():
+        print("Corse number: "+ course[i].get("y"))
+        for i in range (0,len(course)):
+            print("Course ID"+course[i].get("c"))
+            print("Course name"+course[i].get("cbn"))
+
+
+
+class Mark:
+    def input_mark():
+        for i in range(0, len(students)):
+            mark.append({course:mark})
+            mark = m
+            m = float(input("mark: "))
+            mark[i].update({course:mark})
+    def show_mark():
+        for i in range(0,len(students)):
+            print("student mark"+course[i].get("m"))
+
